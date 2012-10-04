2a061997a5b9960b28e8dd463e7a0a355075b612
Kathi <kathibr1706@googlemail.com>
2012-10-04 19:58:42 +0200

    login mit mongo implementiert - funktioniert noch nicht

--- Systemanalyse/src/com/fallstudie/simulation/server/LoginServiceImpl.java
+++ Systemanalyse/src/com/fallstudie/simulation/server/LoginServiceImpl.java
@@ -1,9 +1,18 @@
 package com.fallstudie.simulation.server;
 
 
+import java.net.UnknownHostException;
+
 import com.fallstudie.simulation.client.login.LoginSimulation;
 import com.fallstudie.simulation.client.login.LoginService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
 /**
  * The server side implementation of the RPC service.
  */
@@ -19,7 +28,31 @@ public class LoginServiceImpl extends RemoteServiceServlet implements
 //		String username = LoginSimulation.user1.getUsername();
 //		String kennwort = LoginSimulation.user1.getKennwort();
 		
-		return true;
+		
+		String kennwort = LoginSimulation.user1.getKennwort();
+		String username = LoginSimulation.user1.getUsername();
+		
+//			Mongo mongo = new Mongo("localhost", 27017);
+		
+		DB db = getMongo().getDB("skdns");
+		DBCollection collection = db.getCollection("login");
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("name", username);
+		searchQuery.put("password", kennwort);
+		
+		DBCursor cursor = collection.find(searchQuery);
+		DBObject dbObject = cursor.next();
+		
+		String dbusername = (String) dbObject.get("name");
+		String dbkennwort = (String) dbObject.get("password");
+					
+		if((dbusername==username)&&(dbkennwort==kennwort)){
+			return true;
+		}else return false;
+		
+		
+		//return true;
 		
 	}
 
@@ -28,4 +61,17 @@ public class LoginServiceImpl extends RemoteServiceServlet implements
 		// TODO Auto-generated method stub
 		return null;
 	}
+	private static Mongo getMongo() {
+        Mongo m = null;
+	 
+        try {
+            m = new Mongo("localhost", 27017);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+        return m;
+	}
 }
+
